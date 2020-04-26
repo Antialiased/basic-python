039b613ec76ece40993678dceffacb000fa0bc5e
Antialiased <antialiased@gmail.com>
2020-04-26 14:14:50 +0100

    actually test third exercise

--- basic_python_5.py
+++ basic_python_5.py
@@ -24,8 +24,19 @@ def is_in_english_dictionary(english_words, test_word):
 import random
 words_file = open("words_alpha.txt", 'r')
 words_list = words_file.readlines()
+for i in range(len(words_list)):
+    words_list[i] = words_list[i].rstrip()
 random.shuffle(words_list)
 print_longest_word(words_list)
 print_palindromes(words_list)
 
 import unittest
+
+class TestExercises(unittest.TestCase):
+    def test_exercise_3(self):
+        self.assertTrue(is_in_english_dictionary(words_list), 'apple')
+        self.assertTrue(is_in_english_dictionary(words_list), 'plump')
+        self.assertFalse(is_in_english_dictionary(words_list), 'dapwok')
+        self.assertFalse(is_in_english_dictionary(words_list), '235')
+
+unittest.main()
