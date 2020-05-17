eef1cf91ade6994759d5cb840de96770700aa28e
Antialiased <antialiased@gmail.com>
2020-05-17 18:30:39 +0100

    add movie review dataset

---
+++ basic_python_8.py
@@ -0,0 +1,9 @@
+# Exercise 1.
+# Print out the contents of this file (basic_python_8.py)
+# Hint: you can open and print this file just like any other!
+
+
+# Exercise 2. Open a few imdb review txt files (like 1000 will do) and find the 10 longest words.
+
+
+# Exercise 3. Open all the imdb review files, build a database representing all the individual words and their frequency, and save it for future analysis.
